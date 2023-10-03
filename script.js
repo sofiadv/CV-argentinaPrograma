f3a06aa225a15d0d3cec8a5095b014fcd68bda8c
sofiadv <sofia.dv@gmail.com>
2023-10-03 19:50:33 -0300

    agregué funciones en el js y los atributos correspondientes en el HTML, y mejora de estilo en el CSS

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function ocultar1(){
-    document.getElementById('perfil-personal').style.display = none
-    document.getElementById('perfil-personal').style.display = none
+document.addEventListener('DOMContentLoaded', function() {
+    mostrarSeccion('perfil-personal');
+});
+// muestra y oculta las secciones en el currículum
+function mostrarSeccion(seccionId) {
+    const divs = document.querySelectorAll('main > div');
+
+    divs.forEach(div => {
+        div.style.display = 'none';
+    });
+
+    const divToShow = document.getElementById(seccionId);
+    if (divToShow) {
+        divToShow.style.display = 'block';
+    }
 }
